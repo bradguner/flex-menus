24690118a24e29522b9c6d65a43e9fa3fa4e9c6c
Scott Wallace <ecosse_18@hotmail.com>
2016-03-11 18:08:49 -0500

    added bluetooth and comments to code

---
+++ Menus/app/src/main/java/com/flex/menus/Bluetooth.java
@@ -0,0 +1,12 @@
+package com.flex.menus;
+
+/**
+ * Created by ecosse_18 on 16-03-11.
+ */
+public class Bluetooth {
+
+    // run method which reads the bluetooth input
+    public static void run() {
+
+    }
+}
--- Menus/app/src/main/java/com/flex/menus/MainActivity.java
+++ Menus/app/src/main/java/com/flex/menus/MainActivity.java
@@ -1,23 +1,21 @@
 package com.flex.menus;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements AnimationListener {
 
-    Button moveButton;
-    TextView txtView;
-    ImageView imgView;
+    private Button moveButton;
+    private TextView txtView;
+    private ImageView imgView;
     private boolean menuVisible;
     private LinearLayout LL;
 
@@ -28,23 +26,19 @@ public class MainActivity extends Activity implements AnimationListener {
 
         txtView = (TextView) findViewById(R.id.txtView);
         imgView = (ImageView) findViewById(R.id.imageView);
-
         LL = (LinearLayout) this.findViewById(R.id.MenuLayout);
 
-
-
-
-        //finally
+        // Animations
         final Animation animShow = AnimationUtils.loadAnimation(this, R.anim.slide_in_menu);
         animShow.setAnimationListener(this);
-
         final Animation animHide = AnimationUtils.loadAnimation(this, R.anim.slide_out_menu);
         animHide.setAnimationListener(this);
 
+        // hide menu upon creation
         menuVisible = false;
         imgView.startAnimation(animHide);
 
-
+        // button to mimic bend gesture
         moveButton = (Button) findViewById(R.id.mvBtn);
         moveButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -56,18 +50,19 @@ public class MainActivity extends Activity implements AnimationListener {
                 }
             }
         });
-
-
     }
 
+    // react to a bend and based on what integer was sent react accordingly
+    //public static void reactToBend() {}
 
-
+    // starts animation to display menu
     private void displayMenu(Animation anim) {
         txtView.setText("Showing!");
         imgView.startAnimation(anim);
         menuVisible = true;
     }
 
+    // starts animation to hide menu
     private void hideMenu(Animation anim) {
         txtView.setText("Hidden!");
         imgView.startAnimation(anim);
@@ -81,11 +76,7 @@ public class MainActivity extends Activity implements AnimationListener {
     public void onAnimationRepeat(Animation animation){}
 
     @Override
-    public void onAnimationStart(Animation animation)
-    {
-        // This is the key...
-        //set the coordinates for the bounds (left, top, right, bottom) based on the offset value (50px) in a resource XML
-        LL.layout(0, -(int)this.getResources().getDimension(R.dimen.menu_offset),
-                LL.getWidth(), LL.getHeight() + (int)this.getResources().getDimension(R.dimen.menu_offset));
+    public void onAnimationStart(Animation animation) {
+        LL.layout(0, -(int)this.getResources().getDimension(R.dimen.menu_offset), LL.getWidth(), LL.getHeight() + (int)this.getResources().getDimension(R.dimen.menu_offset));
     }
 }
