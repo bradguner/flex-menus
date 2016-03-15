709a4106f1142c9fbee5d7feeffa420e646d89ba
Scott Wallace <ecosse_18@hotmail.com>
2016-03-14 23:15:03 -0400

    cleaned up dead code

--- Menus/app/src/main/java/com/flex/menus/MainActivity.java
+++ Menus/app/src/main/java/com/flex/menus/MainActivity.java
@@ -18,41 +18,23 @@ import android.widget.TextView;
 public class MainActivity extends Activity  {
 
     private Button moveButton;
-    private TextView txtView;
-    private ImageView imgView;
     private static boolean menuVisible;
     public RelativeLayout RL;
-    Animation animShow;
-    Animation animHide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //txtView = (TextView) findViewById(R.id.txtView);
-       // imgView = (ImageView) findViewById(R.id.imageView);
         RL = (RelativeLayout) findViewById(R.id.rela);
 
-        /*
-        // Animations
-        animShow = AnimationUtils.loadAnimation(this, R.anim.slide_in_menu);
-        animShow.setAnimationListener(this);
-        animHide = AnimationUtils.loadAnimation(this, R.anim.slide_out_menu);
-        animHide.setAnimationListener(this);
-        */
-
-        // hide menu upon creation
         menuVisible = false;
-        //imgView.startAnimation(animHide);
 
         // button to mimic bend gesture
         moveButton = (Button) findViewById(R.id.mvBtn);
         moveButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 if (menuVisible) {
-                    //hideMenu(animHide)
-
                     FragmentManager fragmentManager = getFragmentManager();
                     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                     fragmentTransaction.setCustomAnimations(R.anim.enter_anim, R.anim.exit_anim);
@@ -64,7 +46,6 @@ public class MainActivity extends Activity  {
                     menuVisible = false;
                 }
                 else {
-                    //displayMenu(animShow);
                     FragmentManager fragmentManager = getFragmentManager();
                     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                     fragmentTransaction.setCustomAnimations(R.anim.enter_anim, R.anim.exit_anim);
@@ -73,18 +54,15 @@ public class MainActivity extends Activity  {
                     fragmentTransaction.add(R.id.fragmentContainer, fragment,"generalMenu");
                     fragmentTransaction.commit();
 
-
                     menuVisible = true;
                 }
             }
         });
 
-        // run the bluetooth reading part
-
-    }
+    } // end onCreate
 
 
-
+    // Change Background Methods
     public void changeBackgroundStar(View v){
         RL.setBackgroundResource(R.drawable.star_photo);
     }
@@ -130,30 +108,6 @@ public class MainActivity extends Activity  {
         }
     }
 
-
-    // starts animation to display menu
-    private void displayMenu(Animation anim) {
-        txtView.setText("Showing!");
-        imgView.startAnimation(anim);
-        menuVisible = true;
-    }
-
-    // starts animation to hide menu
-    private void hideMenu(Animation anim) {
-        txtView.setText("Hidden!");
-        imgView.startAnimation(anim);
-        menuVisible = false;
-    }
-
-    @Override
-    public void onAnimationEnd(Animation animation){}
-
-    @Override
-    public void onAnimationRepeat(Animation animation){}
-
-    @Override
-    public void onAnimationStart(Animation animation) {
-        LL.layout(0, -(int)this.getResources().getDimension(R.dimen.menu_offset), LL.getWidth(), LL.getHeight() + (int)this.getResources().getDimension(R.dimen.menu_offset));
-    }
     */
+
 }
