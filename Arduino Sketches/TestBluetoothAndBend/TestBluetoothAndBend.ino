bedd0aefcbc18c21ea4a984b67bfc88e5954d759
Bradley Guner <BradG@Brads-MacBook-Pro.local>
2016-03-21 19:50:09 -0400

    update for housekeeping on information that's sent to the android app
    “#”  write to bluetooth indicates that the broadcasted information is
    completed, sensor[0] is the horizontal bend sensor, sensor[1] is the
    corner bend sensor

--- Arduino Sketches/TestBluetoothAndBend/TestBluetoothAndBend.ino
+++ Arduino Sketches/TestBluetoothAndBend/TestBluetoothAndBend.ino
@@ -1,9 +1,5 @@
-#include <SoftwareSerial.h>
-SoftwareSerial BTserial(12, 11); //RX, TX pins
-
-char c = ' ';
 boolean ready = false;
-int sensor[2] = {0,0};
+byte sensor[2] = {0,0};
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -13,24 +9,34 @@ void setup() {
 void loop() {
   readSensors();
   if (ready) {
-   BTserial.print(sensor[0]);
-   BTserial.print(", ");
-   BTserial.print(sensor[1]);
-   BTserial.println();
+   BTserial.write(sensor[0]);
+   BTserial.write(sensor[1]);
+   BTserial.write("#");
    ready = false;
-   delay(500);
+   sensor[0] = 0;
+   sensor[1] = 0;
+   delay(1000);
   }
 }
 
 void readSensors() {
   int sensor1 = analogRead(A0);
   int sensor2 = analogRead(A1);
-  if (sensor1 >= 610 || sensor1 <= 450 || sensor2 >= 700 || sensor2 <= 280) {
-    sensor[0] = sensor1;
-    sensor[1] = sensor2;
+  if (sensor1 >= 610) {
+    sensor[0] = byte(1);
     ready = true;
   }
-  
-  
+  else if (sensor1 <= 450) {
+    sensor[0] = byte(2);
+    ready = true;
+  }
+  if (sensor2 >= 700) {
+   sensor[1] = byte(1);
+   ready = true;
+  }
+  else if (sensor2 <= 280) {
+   sensor[1] = byte(2);
+   ready = true;
+  }
 }
 
